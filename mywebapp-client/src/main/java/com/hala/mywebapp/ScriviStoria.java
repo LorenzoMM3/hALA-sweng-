f1a057c170b845a9c7c1d56fc24867b2b9f88a49
Teallapesca <133207041+Teallapesca@users.noreply.github.com>
2024-02-29 09:18:46 +0100

    cambiata pagina ScriviStoria in uibinder

--- mywebapp-client/src/main/java/com/hala/mywebapp/ScriviStoria.java
+++ mywebapp-client/src/main/java/com/hala/mywebapp/ScriviStoria.java
@@ -3,94 +3,71 @@ package com.hala.mywebapp;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.*;
+import com.ibm.icu.text.ListFormatter.Style;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.uibinder.client.UiTemplate;
 
-public class ScriviStoria implements IsWidget {
+public class ScriviStoria extends Composite implements IsWidget {
 
-    public HorizontalPanel mainPanel;
-    public static final GreetingServiceAsync hALAServiceAsync = GWT.create(GreetingService.class);
-    protected static String utenteAttivo = "";
+    private static final ScriviStoriaUiBinder uiBinder = GWT.create(ScriviStoriaUiBinder.class);
+   // @UiTemplate("ScriviStoria.ui.xml")
+    interface ScriviStoriaUiBinder extends UiBinder<Widget, ScriviStoria> {}
 
     public ScriviStoria() {
-       mainPanel = GWT.create(HorizontalPanel.class) ;
-       initWidget();
-    }
-
-    private void initWidget() {
-        Label storyLabel = new Label("Scrivi il titolo della storia:");
-        TextBox storyField = new TextBox();
-        Label scenarioInizialeLabel = new Label("Scrivi lo scenario iniziale:");
-        TextBox scenarioInizialeField = new TextBox();
-        Label primoScenarioLabel = new Label("Scrivi il primo scenario intermedio:");
-        TextBox primoScenarioField = new TextBox();
-        Button aggiungiIntermedio = new Button("aggiungi uno scenario intermedio");
-        Label scenarioFinaleLabel = new Label("scrivi il primo scenario finale:");
-        TextBox scenarioFinaleField = new TextBox();
-        Button aggiungiFinale = new Button("aggiungi uno scenario finale");
-        
-        Label messageLabel = new Label();
-        Button backButton = new Button("Back");
-
-        VerticalPanel vp = GWT.create(VerticalPanel.class);
-        VerticalPanel vpIniziale = GWT.create(VerticalPanel.class);
-        VerticalPanel vpIntermedio = GWT.create(VerticalPanel.class);
-        VerticalPanel vpFinale = GWT.create(VerticalPanel.class);
-        vp.add(storyLabel);
-        vp.add(storyField);
-        vpIniziale.add(scenarioInizialeLabel);
-        vpIniziale.add(scenarioInizialeField);
-        vpIntermedio.add(primoScenarioLabel);
-        vpIntermedio.add(primoScenarioField);
-        vpIntermedio.add(aggiungiIntermedio);
-        vpFinale.add(scenarioFinaleLabel);
-        vpFinale.add(scenarioFinaleField);
-        vpFinale.add(aggiungiFinale);
-        vp.add(backButton);
-
-        vp.setSpacing(30); //Spazio tra bottoni
-        vpIniziale.setSpacing(30); //Spazio tra bottoni
-        vpIntermedio.setSpacing(30); //Spazio tra bottoni
-        vpFinale.setSpacing(30); //Spazio tra bottoni
-
-        mainPanel.add(vp);
-        mainPanel.add(vpIniziale);
-        mainPanel.add(vpIntermedio);
-        mainPanel.add(vpFinale);
-        mainPanel.setSpacing(100); //Spazio tra i bottoni
-        
-		aggiungiIntermedio.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-               TextBox newTextBox = new TextBox();
-                
-                // Aggiungi la casella di testo al pannello radice della pagina
-                vpIntermedio.add(newTextBox);
-            }
-        });
-
-        aggiungiFinale.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-               TextBox newTextBox = new TextBox();
-                
-                // Aggiungi la casella di testo al pannello radice della pagina
-                vpFinale.add(newTextBox);
-            }
-        });
-
-
-        backButton.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                RootPanel.get("startTable").clear();
-                RootPanel.get("startTable").add(new Login());
-            }
-        });
+        initWidget(uiBinder.createAndBindUi(this));
+        //loginButton.getElement().getStyle().setColor("red");
+       /* titoloStoria.getElement().getStyle().setProperty("gridColumn", "5");
+        titoloStoria.getElement().getStyle().setProperty("gridRow", "2");
+        titoloStoria.getElement().getStyle().setProperty("justifySelf", "start");*/
 
     }
-        
+
+    @UiField
+    TextBox titoloStoria;
+
+    /*@UiHandler("signinButton")
+    void onClickSignin(ClickEvent event) {
+        RootPanel.get("startTable").clear();
+        RootPanel.get("startTable").add(new SignIn());
+    }
+
+    @UiHandler("loginButton")
+    public void onClickLogin(ClickEvent event) {
+        RootPanel.get("startTable").clear();
+        RootPanel.get("startTable").add(new Login());
+    }
+
+    @UiHandler("logoutButton")
+    public void onClickLogout(ClickEvent event) {
+        RootPanel.get("startTable").clear();
+        RootPanel.get("startTable").add(new Logout());
+    }
+
 
     @Override
     public Widget asWidget() {
-        return mainPanel;
-     }
+        return this;
+    }*/
 }
